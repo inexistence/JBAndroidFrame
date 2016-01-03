7512edd0e64095ca8c75a68a0a4880b227d6e533
黄建斌 <inexistenceorz@gmail.com>
2016-01-03 23:50:15 +0800

    提取日期功能类

---
+++ app/src/main/java/com/jb/androidframe/utils/DateUtils.java
@@ -0,0 +1,149 @@
+package com.jb.androidframe.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Created by Jianbin on 2016/1/3.
+ */
+public class DateUtils {
+
+    /**
+     * 返回当前系统时间
+     */
+    public static String getDataTime(String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        return df.format(new Date());
+    }
+
+    private final static ThreadLocal<SimpleDateFormat> dateFormater = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        }
+    };
+
+    private final static ThreadLocal<SimpleDateFormat> dateFormater2 = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd",  Locale.getDefault());
+        }
+    };
+
+
+    public static String friendlyTime(Date time) {
+        if (time == null) {
+            return "Unknown";
+        }
+        if (!isInEasternEightZones()) {
+            time = transformTime(time, TimeZone.getTimeZone("GMT+08"),
+                    TimeZone.getDefault());
+        }
+        String ftime = "";
+        Calendar cal = Calendar.getInstance();
+
+        // 判断是否是同一天
+        String curDate = dateFormater2.get().format(cal.getTime());
+        String paramDate = dateFormater2.get().format(time);
+        if (curDate.equals(paramDate)) {
+            int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
+            if (hour == 0)
+                ftime = Math.max(
+                        (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
+                        + "分钟前";
+            else
+                ftime = hour + "小时前";
+            return ftime;
+        }
+
+        long lt = time.getTime() / 86400000;
+        long ct = cal.getTimeInMillis() / 86400000;
+        int days = (int) (ct - lt);
+        if (days == 0) {
+            int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
+            if (hour == 0)
+                ftime = Math.max(
+                        (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
+                        + "分钟前";
+            else
+                ftime = hour + "小时前";
+        } else if (days == 1) {
+            ftime = "昨天";
+        } else if (days == 2) {
+            ftime = "前天 ";
+        } else if (days > 2 && days < 31) {
+            ftime = days + "天前";
+        } else if (days >= 31 && days <= 2 * 31) {
+            ftime = "一个月前";
+        } else if (days > 2 * 31 && days <= 3 * 31) {
+            ftime = "2个月前";
+        } else if (days > 3 * 31 && days <= 4 * 31) {
+            ftime = "3个月前";
+        } else {
+            ftime = dateFormater2.get().format(time);
+        }
+        return ftime;
+    }
+
+    /**
+     * 以友好的方式显示时间
+     *
+     * @param sdate
+     * @return
+     */
+    public static String friendlyTime(String sdate) {
+        Date time = toDate(sdate);
+        return friendlyTime(time);
+    }
+
+    /**
+     * 将字符串转位日期类型
+     *
+     * @param sdate
+     * @return
+     */
+    public static Date toDate(String sdate) {
+        return toDate(sdate, dateFormater.get());
+    }
+
+    public static Date toDate(String sdate, SimpleDateFormat dateFormater) {
+        try {
+            return dateFormater.parse(sdate);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 判断用户的设备时区是否为东八区（中国） 2014年7月31日
+     *
+     * @return
+     */
+    public static boolean isInEasternEightZones() {
+        boolean defaultVaule = true;
+        if (TimeZone.getDefault() == TimeZone.getTimeZone("GMT+08"))
+            defaultVaule = true;
+        else
+            defaultVaule = false;
+        return defaultVaule;
+    }
+
+    /**
+     * 根据不同时区，转换时间 2014年7月31日
+     */
+    public static Date transformTime(Date date, TimeZone oldZone,
+                                     TimeZone newZone) {
+        Date finalDate = null;
+        if (date != null) {
+            int timeOffset = oldZone.getOffset(date.getTime())
+                    - newZone.getOffset(date.getTime());
+            finalDate = new Date(date.getTime() - timeOffset);
+        }
+        return finalDate;
+    }
+
+}
